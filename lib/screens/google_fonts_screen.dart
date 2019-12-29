6dbea6b7c0fbe904b74c3f7b196c52fe19d051d7
Dhanasekar <sendmailtodhana@gmail.com>
2019-12-29 19:32:14 +0530

    google fonts updated seperate screen

--- lib/screens/google_fonts_screen.dart
+++ lib/screens/google_fonts_screen.dart
@@ -3,12 +3,61 @@ import 'package:flutterground/widgets/drawer_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class GoogleFontsScreen extends StatelessWidget {
+  String _subtitle = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar : AppBar(title: Text('Google Fonts'),),
+      appBar: AppBar(
+        title: Text(
+          'Google Fonts',
+          style: GoogleFonts.fruktur(
+            textStyle: TextStyle(color: Colors.amber),
+            fontSize: 25,
+            fontWeight: FontWeight.w400,
+            fontStyle: FontStyle.normal,
+          ),
+        ),
+      ),
       drawer: DrawerWidget(),
-      
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text(
+                "loveYaLikeASister",
+                style: GoogleFonts.loveYaLikeASister(),
+              ),
+              subtitle: Text(_subtitle, style: GoogleFonts.loveYaLikeASister()),
+            ),
+            Divider(
+              thickness: 1,
+            ),
+            ListTile(
+              title: Text(
+                "gabriela",
+                style: GoogleFonts.gabriela(),
+              ),
+              subtitle: Text(_subtitle, style: GoogleFonts.gabriela()),
+            ),
+            Divider(
+              thickness: 1,
+            ),
+            ListTile(
+              title: Text(
+                "herrVonMuellerhoff",
+                style: GoogleFonts.herrVonMuellerhoff(),
+              ),
+              subtitle: Text(
+                _subtitle,
+                style: GoogleFonts.herrVonMuellerhoff(),
+              ),
+            ),
+            Divider(
+              thickness: 1,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
