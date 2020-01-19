b14e27ccb71f0b7b1ef6dbdce6c81c60d8f40d12
Dhanasekar <sendmailtodhana@gmail.com>
2020-01-19 22:44:50 +0530

    async and future added

--- lib/providers/forms_provider.dart
+++ lib/providers/forms_provider.dart
@@ -2,27 +2,24 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-
-import '../data/strings_data.dart' ;
+import '../data/strings_data.dart';
 import '../model/user.dart';
 
-
 class FormsProvider {
-
-  Future<void> submitUserData( UserDetail userDetail){
-    return  http.post(StringsData.userDB, body: json.encode({
-      "username" : userDetail.username,
-      "password" : userDetail.password,
-      "dob" : userDetail.dob,
-      "mobileNo" : userDetail.mobileNo,
-      "emailId" : userDetail.emailId
-
-    })).then( ( response ){
-      print("success" + StringsData.userDB);
-      print( json.decode(response.body));
-    }).catchError((e) {
-      print(e);
-    });
+  Future<void> submitUserData(UserDetail userDetail) async {
+    try {
+      final response = await http.post(StringsData.userDB,
+          body: json.encode({
+            "username": userDetail.username,
+            "password": userDetail.password,
+            "dob": userDetail.dob,
+            "mobileNo": userDetail.mobileNo,
+            "emailId": userDetail.emailId
+          }));
+          print(response.body);
+    } catch (error) {
+      print("error " + error);
+      throw error;
+    }
   }
-
 }
